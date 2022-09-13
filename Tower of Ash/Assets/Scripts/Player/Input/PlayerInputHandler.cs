2b22f805c7e99fc70f785dec98ce013b6ca9e046
Kwiper <jonny.tibboh@gmail.com>
2022-09-13 09:51:56 -0700

    Set up basic new input system on Player object

---
+++ Tower of Ash/Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private Vector2 movementInput;
+
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        //Debug.Log("Move input");
+        movementInput = context.ReadValue<Vector2>();
+        Debug.Log(movementInput);
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Jump button pushed down now");
+        }
+
+        if (context.performed)
+        {
+            Debug.Log("Jump button being held down now");
+        }
+
+        if (context.canceled)
+        {
+            Debug.Log("Jump button has been released");
+        }
+
+        //Debug.Log("Jump input");
+    }
+
+}
