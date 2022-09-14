1769202cf75e51c2945848de9ce0c0c72e77f6ee
Kwiper <jonny.tibboh@gmail.com>
2022-09-13 18:03:36 -0700

    Finished jumping logic
    + Added double jump
    + Added coyote time
    + Added variable jump height

--- Tower of Ash/Assets/Scripts/Player/PlayerStates/SubStates/PlayerInAirState.cs
+++ Tower of Ash/Assets/Scripts/Player/PlayerStates/SubStates/PlayerInAirState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.TerrainTools;
 using UnityEngine;
 
 public class PlayerInAirState : PlayerState {
@@ -7,7 +8,10 @@ public class PlayerInAirState : PlayerState {
     private int xInput;
 
     private bool isGrounded;
-
+    private bool jumpInput;
+    private bool jumpInputStop;
+    private bool coyoteTime;
+    private bool isJumping;
 
     public PlayerInAirState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
     {
@@ -34,12 +38,22 @@ public class PlayerInAirState : PlayerState {
     {
         base.LogicUpdate();
 
+        CheckCoyoteTime();
+
         xInput = player.InputHandler.NormInputX;
+        jumpInput = player.InputHandler.JumpInput;
+        jumpInputStop = player.InputHandler.JumpInputStop;
+
+        CheckJumpMultiplier();
 
         if(isGrounded && player.CurrentVelocity.y < 0.01f)
         {
             stateMachine.ChangeState(player.LandState);
-        } 
+        }
+        else if(jumpInput && player.JumpState.CanJump())
+        {
+            stateMachine.ChangeState(player.JumpState);
+        }
         else
         {
             player.CheckIfShouldFlip(xInput);
@@ -50,8 +64,37 @@ public class PlayerInAirState : PlayerState {
         }
     }
 
+    private void CheckJumpMultiplier()
+    {
+        if (isJumping)
+        {
+            if (jumpInputStop)
+            {
+                player.SetVelocityY(player.CurrentVelocity.y * playerData.variableJumpHeightMultiplier);
+                isJumping = false;
+            }
+            else if (player.CurrentVelocity.y <= 0)
+            {
+                isJumping = false;
+            }
+        }
+    }
+
     public override void PhysicsUpdate()
     {
         base.PhysicsUpdate();
     }
+
+    private void CheckCoyoteTime()
+    {
+        if(coyoteTime && Time.time > startTime + playerData.coyoteTime)
+        {
+            coyoteTime = false;
+            player.JumpState.DecreaseAmountOfJumpsLeft();
+        }
+    }
+
+    public void StartCoyoteTime() => coyoteTime = true;
+
+    public void SetIsJumping() => isJumping = true;
 }
