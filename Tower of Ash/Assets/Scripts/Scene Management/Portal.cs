dcf9f06c45fdd82436f1669c52c1c24437798c08
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2022-09-26 14:06:32 -0700

    Added Portal Script and Object

---
+++ Tower of Ash/Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour //IplayerTriggerable
+{
+    /*
+    public void OnPlayerTriggered(Player play)
+    {
+        Debug.Log("Player entered portal")
+    }
+    */
+}
