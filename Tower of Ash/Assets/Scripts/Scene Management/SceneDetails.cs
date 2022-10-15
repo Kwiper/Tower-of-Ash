78a2b9d5449bd936cc3ccaaee1f33f20e993e35e
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2022-10-14 18:02:54 -0700

    Added Additive Loading
    -Additive Loading now implemented with level implementation
    -Scene triggers that cause the additive loading to trigger
    -Added a small platform the gameplay scene to stop the player from falling through before the level loads

---
+++ Tower of Ash/Assets/Scripts/Scene Management/SceneDetails.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneDetails : MonoBehaviour
+{
+    [SerializeField] List<SceneDetails> connectedScenes;
+    public bool IsLoaded = false;
+
+
+    //GameObject Play;
+    
+    Player Player;
+    GameObject Play;
+    private void Awake()
+    {
+        Play =  GameObject.FindGameObjectsWithTag("Player")[0];
+        Player = Play.GetComponent<Player>();
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.tag == "Player"){
+
+            Debug.Log($"Entered {gameObject.name}");
+            LoadScene();
+            Player.SetCurrentScene(this);
+
+            //Load all connected scenes
+            foreach (var scene in connectedScenes)
+            {
+                scene.LoadScene();
+            }
+            
+            //Unload the scenes that are no longer connected
+            if (Player.PrevScene != null){
+                var previouslyLoadedScenes = Player.PrevScene.connectedScenes;
+
+                foreach (var scene in previouslyLoadedScenes)
+                {
+                    if(!connectedScenes.Contains(scene) && scene != this){
+                        scene.UnloadScene();
+                    }
+                }
+            }
+        }
+    }
+
+   
+
+
+    public void LoadScene(){
+            if(!IsLoaded){
+                SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+                IsLoaded = true;
+            }
+    }
+
+    public void UnloadScene(){
+            
+            if(IsLoaded){
+                SceneManager.UnloadSceneAsync(gameObject.name);
+                IsLoaded = false;
+            }
+    }    
+}
