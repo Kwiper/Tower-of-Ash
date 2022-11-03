2f18b8b27bb293e4082a454170d7787adafc16c2
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2022-11-02 21:07:49 -0700

    Alpha Build Ready to go
    -Fixed some enemies,
    -added a lot of backgrounds
    -Added tinder Caches
    -Corrected playerData file
    -Added Room with two more statues in it for test purposes

--- Tower of Ash/Assets/Scripts/Gameplay/TinderCache.cs
+++ Tower of Ash/Assets/Scripts/Gameplay/TinderCache.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class TinderCache : MonoBehaviour
 {
+    [SerializeField]
+    private PlayerData playerData;
+    [SerializeField]
+    private int tinderReward = 50;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {	
-	        //FindObjectOfType<OyxgenManager>().oxygenRegenPickup();
+	        playerData.tinder = playerData.tinder + tinderReward;
 	        GetComponent<BoxCollider2D>().enabled = false;
 	        GetComponent<SpriteRenderer>().enabled = false;
         }    
