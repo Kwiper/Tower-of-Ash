0f80478e60a7a4db410f29983bc7728b8a172462
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2022-11-22 05:51:36 -0800

    Enemy spawners now intake rotation as a value

--- Tower of Ash/Assets/Scripts/Scene Management/EnemySpawner.cs
+++ Tower of Ash/Assets/Scripts/Scene Management/EnemySpawner.cs
@@ -6,16 +6,20 @@ public class EnemySpawner: MonoBehaviour
 {
     [SerializeField] 
     private GameObject enemyPrefab;
+    [SerializeField] 
+    private float rotationAngle = 0;
     private GameObject enemy;
     public bool isDead;
     public bool isSpawned = false;
     private Transform pos;
 
     public void SpawnEnemy(){
+        var rot = new Vector3(0,0,rotationAngle);
         pos = GetComponent<Transform>();
         pos.position = new Vector3(pos.position.x, pos.position.y, -1);
         enemy = Instantiate(enemyPrefab, pos.position, Quaternion.identity);
-        
+        enemy.GetComponent<Transform>().Rotate(rot, Space.Self);
+        //enemy.Transform.Rotate(rot, Space.Self);
     }
 
     public void DespawnEnemy(){
