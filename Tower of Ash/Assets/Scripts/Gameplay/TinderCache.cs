b451f31622d74aed696e4a6a62a64522beb48863
ItalianCannolis <61303396+ItalianCannolis@users.noreply.github.com>
2022-11-24 21:54:56 -0800

    Tinder cache now records it's location to playerData

--- Tower of Ash/Assets/Scripts/Gameplay/TinderCache.cs
+++ Tower of Ash/Assets/Scripts/Gameplay/TinderCache.cs
@@ -8,14 +8,25 @@ public class TinderCache : MonoBehaviour
     private PlayerData playerData;
     [SerializeField]
     private int tinderReward = 50;
+    //private bool isCollected = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {	
 	        playerData.tinder = playerData.tinder + tinderReward;
+            var pos = new Vector2(gameObject.GetComponent<Transform>().position.x,gameObject.GetComponent<Transform>().position.y);
+            playerData.CollectedTinderCacheLocations.Add(pos);
 	        GetComponent<BoxCollider2D>().enabled = false;
 	        GetComponent<SpriteRenderer>().enabled = false;
         }    
     }
+
+    public void setTinderReward(int tinderRewardVal){
+        tinderReward = tinderRewardVal;
+    }
+
+    public void setPlayerData(PlayerData playData){
+        playerData = playData;
+    }   
 }
