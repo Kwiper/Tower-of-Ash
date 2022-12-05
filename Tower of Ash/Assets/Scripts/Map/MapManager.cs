31be720b1e37da2845eb285c8f296f2dea5b6645
Kwiper <jonny.tibboh@gmail.com>
2022-12-05 12:46:45 -0800

    Hold tab to open map implemented

---
+++ Tower of Ash/Assets/Scripts/Map/MapManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+
+    Player player;
+
+    [SerializeField]
+    GameObject[] children;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        Debug.Log(player.InputHandler.MapInput);
+
+        if (player.InputHandler.MapInput)
+        {
+            
+            foreach(GameObject child in children)
+            {
+                child.SetActive(true);
+            }
+            
+        }
+        else
+        {
+            
+            foreach(GameObject child in children)
+            {
+                child.SetActive(false);
+            }
+            
+        }
+    }
+
+
+}
