acddf0debcd5efa84dfe8074e9adf93f80edea5c
Kwiper <jonny.tibboh@gmail.com>
2022-12-06 15:22:59 -0800

    Basic pause menu

--- Tower of Ash/Assets/Scripts/Menu/PauseMenu.cs
+++ Tower of Ash/Assets/Scripts/Menu/PauseMenu.cs
@@ -6,6 +6,8 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
 
+    public GameObject pauseMenuUI;
+
     Player player;
 
     private void Start()
@@ -15,8 +17,33 @@ public class PauseMenu : MonoBehaviour
 
     private void Update()
     {
-        
+        if (player.InputHandler.PauseInput)
+        {
+            player.InputHandler.UsePauseInput();
+
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
+    void Resume()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
 
 }
