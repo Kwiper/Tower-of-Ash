b094536addb47868a05797e8a04cedbd2b81323a
Kwiper <jonny.tibboh@gmail.com>
2022-12-06 17:11:34 -0800

    Overhauled tutorialization for abilities

--- Tower of Ash/Assets/Scripts/Menu/TutorialMenu.cs
+++ Tower of Ash/Assets/Scripts/Menu/TutorialMenu.cs
@@ -5,25 +5,63 @@ using UnityEngine.InputSystem;
 
 public class TutorialMenu : MonoBehaviour
 {
-    GameObject player;
+    GameObject playerObject;
+
+    Player player;
 
     PlayerInput playerInput;
+
+    [SerializeField]
+    GameObject[] keyboardTexts;
+    [SerializeField]
+    GameObject[] controllerTexts;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = FindObjectOfType<Player>().gameObject;
-        playerInput = player.GetComponent<PlayerInput>();
+        playerObject = FindObjectOfType<Player>().gameObject;
+        player = playerObject.GetComponent<Player>();
+        playerInput = playerObject.GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.InputHandler.InteractInput)
+        {
+            player.InputHandler.UseInteractInput();
+            PauseMenu.GameIsPaused = false;
+            Time.timeScale = 1f;
+            gameObject.SetActive(false);
+        }
+
+        if (playerInput.currentControlScheme == "Keyboard")
+        {
+            foreach (GameObject i in keyboardTexts)
+            {
+                i.gameObject.SetActive(true);
+            }
+            foreach (GameObject i in controllerTexts)
+            {
+                i.gameObject.SetActive(false);
+            }
+        }
+        else if (playerInput.currentControlScheme == "Gamepad")
+        {
+            foreach (GameObject i in keyboardTexts)
+            {
+                i.gameObject.SetActive(false);
+            }
+            foreach (GameObject i in controllerTexts)
+            {
+                i.gameObject.SetActive(true);
+            }
+        }
     }
 
     private void OnEnable()
     {
-        
+        Time.timeScale = 0f;
+        PauseMenu.GameIsPaused = true;
     }
 }
